17b3925b60243647291ff6d5da24b969b1cd4b1e
Mukhametshin, Azat <0402man@gmail.com>
2024-09-04 22:43:41 +0200

    my_malloc and my_free: debug

--- queue.c
+++ queue.c
@@ -17,7 +17,7 @@ typedef struct{
   int start;
 } Q;
 
-unsigned char data[2048];
+unsigned char data[DATA_MAX];
 
 bool is_inited = false;
 
@@ -94,11 +94,11 @@ void my_memcpy(Header* header, unsigned char* buf){
 }
 
 Header* my_malloc(int size){
-  // Best fit strategy
+  // 1st find suitable chunk using best fit strategy
   Header* header = NULL;
   int last_dif = 999999;
   for(int i=Q_MAX*sizeof(Q); i<DATA_MAX;){
-    Header* tmp = (Header*)&data[i];
+    Header* tmp = (Header*)(data + i);
     if(tmp->size == -1 && tmp->capacity >= size && 
        tmp->capacity < last_dif){
       last_dif = tmp->capacity - tmp->size;
@@ -115,7 +115,7 @@ Header* my_malloc(int size){
     on_out_of_memory();
   }
 
-  // if header->size != -1, re-organize the memory.
+  // 2nd if header->size != -1, re-organize the chunk (the queue).
   if(header->size != -1){
     unsigned char buf[header->size];
     my_memcpy(header, buf);
@@ -128,19 +128,46 @@ Header* my_malloc(int size){
       ((unsigned char*)header)[sizeof(Header) + i] = buf[i];
     }
     header->head = 0;
-
-    Header* tmp = header;
-    int last_capacity = tmp->capacity;
-    tmp->capacity = tmp->size + GAP_SIZE;
-
-    header = (Header*) ((unsigned char*)tmp + sizeof(Header) + tmp->capacity);
-    header->capacity = last_capacity - tmp->capacity - sizeof(Header);
   }
-  return header;
+
+  // 3rd slice the chunk into: 
+  // 1. prev_chunk (prev_size+GAP_SIZE) if prev_size != -1 
+  // 2. cur_chunk (>=size)
+  // 3. remainder (if possible)
+  Header* cur_chunk;
+  Header* prev_chunk = header;
+  int free_space;
+  if(prev_chunk->size == -1){
+    cur_chunk = header;
+    free_space = cur_chunk->capacity;
+  }else{
+    int last_capacity = prev_chunk->capacity;
+    prev_chunk->capacity = prev_chunk->size + GAP_SIZE;
+    free_space = last_capacity - prev_chunk->capacity - sizeof(Header);
+
+    cur_chunk = (Header*) 
+          ((unsigned char*)prev_chunk + sizeof(Header) + prev_chunk->capacity);
+  }
+  
+  // int free_space = last_capacity - prev_chunk->capacity - sizeof(Header);
+  if(free_space > size + sizeof(Header)){
+    cur_chunk->capacity = size;
+
+    Header* remainder = (Header*)
+            ((unsigned char*)cur_chunk + sizeof(Header) + size);
+    remainder->size = -1;
+    remainder->head = 0;
+    remainder->capacity = free_space - size - sizeof(Header);
+  }else{
+    cur_chunk->capacity = free_space;
+  }
+  
+  return cur_chunk;
 }
 
+// Deactivates the given chunk and merges it with 2 neigbours (if unused)
 void my_free(Header* header){
-  // TODO: implement me
+  // 1st find the chunk that goes before the given one
   Header* prev_header = NULL;
   for(int i=Q_MAX*sizeof(Q); i<DATA_MAX;){
     Header* tmp = (Header*)(data + i);
@@ -151,9 +178,26 @@ void my_free(Header* header){
     prev_header = tmp;
   }
 
-  if(prev_header == NULL){
-    prev_header->size = -1;
-  }else{
+  // 2nd find the chunk following the given one
+  Header* next_header;
+  next_header = (Header*)
+                ((unsigned char*)header + sizeof(Header) + header->capacity);
+  if((unsigned char*)next_header - data >= DATA_MAX){
+    next_header = NULL;
+  }
+
+  // 3rd deactivate the given chunk
+  header->size = -1;
+  header->head = 0;
+
+  // 4th merge current and following chunks (if unused)
+  if(next_header != NULL && next_header->size == -1){
+    header->capacity = next_header->capacity + sizeof(Header) + 
+                       header->capacity;
+  }
+
+  // 5th merge previous and current chunks (if unused)
+  if(prev_header != NULL && prev_header->size == -1){
     unsigned char buf[prev_header->size];
     my_memcpy(prev_header, buf);
     for(int i=0; i<prev_header->size; i++){
@@ -162,11 +206,6 @@ void my_free(Header* header){
     prev_header->head = 0;
     prev_header->capacity = prev_header->capacity + sizeof(Header) + 
                             header->capacity;
-    
-    // The following is done so that the freed header is seen in the memory
-    header->size = -1;
-    header->capacity = 0;
-    header->head = 0;
   }
 
   return;
@@ -179,9 +218,10 @@ Q *create_queue(){
       ((Q*)data)[j].start = -1;
       cur+=4;
     }
-    ((Header*)&data[cur])->capacity = DATA_MAX - Q_MAX * sizeof(Q);
-    ((Header*)&data[cur])->size = -1;
-    ((Header*)&data[cur])->head = 0;
+    ((Header*)(data+cur))->capacity = 
+                           DATA_MAX - Q_MAX * sizeof(Q) - sizeof(Header);
+    ((Header*)(data+cur))->size = -1;
+    ((Header*)(data+cur))->head = 0;
     cur += sizeof(Header);
     for(; cur<DATA_MAX; cur++){
       data[cur] = 0;
@@ -244,7 +284,7 @@ unsigned char dequeue_byte(Q *q){
     on_illegal_operation();
   }
   if(header->size < 0){
-    fprintf(stderr, "ERROR: header has negative size!");
+    fprintf(stderr, "ERROR: Header has negative size!");
     on_illegal_operation();
   }
 
